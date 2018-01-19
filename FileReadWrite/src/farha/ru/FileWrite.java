94dd54e97001583b2c8ab61adc2c895b9064fc60
FarhaIslam <farhaislam999@gamil.com>
2018-01-19 09:03:42 -0800

    File Write complete

--- FileReadWrite/src/farha/ru/FileWrite.java
+++ FileReadWrite/src/farha/ru/FileWrite.java
@@ -1,7 +1,24 @@
 package farha.ru;
-
+import java.io.*;
 public class FileWrite {
 	
+	public static void main(String[] args) {
+		try {
+            
+            FileWriter writer = new FileWriter("farha.txt", true);
+        
+            writer.write("Hello World");
+            writer.write("\r\n");   
+            writer.write("Good Bye!");
+            writer.close();
+         }
+         
+         catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+
+	}
 
 
 		
