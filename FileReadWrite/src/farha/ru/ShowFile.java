0c427cb82732c6516f475112af0ae714a9e15949
FarhaIslam <farhaislam999@gamil.com>
2018-01-19 09:08:39 -0800

    This class show the file which is previously created.

--- FileReadWrite/src/farha/ru/ShowFile.java
+++ FileReadWrite/src/farha/ru/ShowFile.java
@@ -1,5 +1,24 @@
 package farha.ru;
-
+import java.io.*;
 public class ShowFile {
+	
+	public static void main(String[] args) {
+        
+		try {
 
+            FileInputStream fin = new FileInputStream("farha.txt") ;
+            int character;
+
+            while ((character = fin.read()) != -1) {
+                System.out.print((char) character);
+            
+            }
+            
+            fin.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        
+        }
+    }
 }
